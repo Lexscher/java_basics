8eeb587bf3246e83545396aa08f1fc1a2d22a50d
Alexander Schelchere <ascellie@gmail.com>
2019-09-25 08:30:03 -0400

    Scanner for user input

--- Practice/Practice.java
+++ Practice/Practice.java
@@ -1,11 +1,23 @@
 package Practice;
+import java.util.Scanner;
 
 public class Practice {
   public static void main(String[] args) {
-    int num1 = 5;
-    int num2 = 3;
-    double product = (double) num1 / (double) num2;
+    Scanner sc = new Scanner(System.in);
 
-    System.out.println("The product of " + num1 + " and " + num2 + " is " + product + ".");
+    System.out.print("What is your name? ");
+    String name = sc.nextLine();
+    sc.close();
+    
+    System.out.println("Nice to meet you, " + name);
+
+    
   }
 }
+
+/* PRIMITIVE TYPES & CASTING */
+// int num1 = 5; int num2 = 3; double product = (double) num1 / (double) num2;
+//
+// System.out.println("The product of " + num1 + " and " + num2 + " is " +
+// product + ".");
+//
