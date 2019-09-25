8bfd433503fbadb06d5ba68a3066396140a53c2c
Alexander Schelchere <ascellie@gmail.com>
2019-09-25 08:18:33 -0400

    primitive types & casting

---
+++ Practice/Practice.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Practice {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 3;
+    double product = (double) num1 / (double) num2;
+
+    System.out.println("The product of " + num1 + " and " + num2 + " is " + product + ".");
+  }
+}
