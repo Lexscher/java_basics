14f1f2f28485a144ca07d73c8ad17aa20cbd6271
Alexander Schelchere <ascellie@gmail.com>
2019-09-26 07:01:28 -0400

    if/else statements

--- Practice/Practice.java
+++ Practice/Practice.java
@@ -1,23 +1,44 @@
 package Practice;
-import java.util.Scanner;
 
 public class Practice {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    String movie = "The Dark Knight";
 
-    System.out.print("What is your name? ");
-    String name = sc.nextLine();
-    sc.close();
-    
-    System.out.println("Nice to meet you, " + name);
+    char d = movie.charAt(4);
 
-    
+    System.out.println(d + " is the first letter in the second word of " + movie);
+
+    boolean noble = movie.contains("Knight");
+
+    if (noble) {
+      System.out.println("We love " + movie);
+    } else {
+      System.out.println(movie + " wasn't even that good...");
+    }
+
+    if (Math.abs("g".compareTo("n")) > Math.abs("g".compareTo("c"))) {
+      System.out.println("The difference between \"g\" and \"n\", " + Math.abs("g".compareTo("n"))
+          + " is greater than that of \"g\" and \"c\", which is " + Math.abs("g".compareTo("c")) + ".");
+    } else {
+      System.out.println("The difference between \"g\" and \"n\", " + Math.abs("g".compareTo("n"))
+          + " is less than that of \"g\" and \"c\", which is " + Math.abs("g".compareTo("c")) + ".");
+    }
+
   }
 }
 
 /* PRIMITIVE TYPES & CASTING */
-// int num1 = 5; int num2 = 3; double product = (double) num1 / (double) num2;
+// int num1 = 5; int num2 = 3; double quotient = (double) num1 / (double) num2;
 //
-// System.out.println("The product of " + num1 + " and " + num2 + " is " +
-// product + ".");
+// System.out.println("The quotient of " + num1 + " and " + num2 + " is " +
+// quotient + ".");
 //
+
+/* Scanner for User Input */
+// Scanner sc = new Scanner(System.in);
+
+// System.out.print("What is your name? ");
+// String name = sc.nextLine();
+// sc.close();
+
+// System.out.println("Nice to meet you, " + name);
